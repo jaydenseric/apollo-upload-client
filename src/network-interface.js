d832717f3a0c36bd8dedcc81f223e0341e932566
Thomas Vogel <thomas@arkulpa.at>
2017-04-26 03:26:14 +0200

    removed prettier formatting

--- src/network-interface.js
+++ src/network-interface.js
@@ -1,40 +1,38 @@
-import {HTTPFetchNetworkInterface, printAST} from 'apollo-client';
-import {extractRequestFiles} from './helpers';
+import {HTTPFetchNetworkInterface, printAST} from 'apollo-client'
+import {extractRequestFiles} from './helpers'
 
 export class HTTPUploadNetworkInterface extends HTTPFetchNetworkInterface {
-  fetchFromRemoteEndpoint({request, options}) {
+  fetchFromRemoteEndpoint ({request, options}) {
     // Skip upload proccess if SSR
     if (typeof window !== 'undefined') {
       // Extract any files from the request
-      const {operation, files} = extractRequestFiles(request);
+      const {operation, files} = extractRequestFiles(request)
 
       // Only initiate a multipart form request if there are uploads
       if (files.length) {
         // Convert query AST to string for transport
-        operation.query = printAST(operation.query);
+        operation.query = printAST(operation.query)
 
         // Build the form
-        const formData = new window.FormData();
-        formData.append('operations', JSON.stringify(operation));
-        files.forEach(({variablesPath, file}) =>
-          formData.append(variablesPath, file)
-        );
+        const formData = new window.FormData()
+        formData.append('operations', JSON.stringify(operation))
+        files.forEach(({variablesPath, file}) => formData.append(variablesPath, file))
 
         // Send request
         return window.fetch(this._uri, {
           method: 'POST',
           body: formData,
           ...options.opts,
-          ...options,
-        });
+          ...options
+        })
       }
     }
 
     // Standard fetch method fallback
-    return super.fetchFromRemoteEndpoint({request, options});
+    return super.fetchFromRemoteEndpoint({request, options})
   }
 }
 
-export function createNetworkInterface({uri, opts = {}}) {
-  return new HTTPUploadNetworkInterface(uri, opts);
+export function createNetworkInterface ({uri, opts = {}}) {
+  return new HTTPUploadNetworkInterface(uri, opts)
 }
