a5065efce89230ee6010fa44a47a5226cd60df42
Guten Ye <ywzhaifei@gmail.com>
2017-05-25 20:17:49 +0800

    Fix node.path undefined bug

--- src/helpers.js
+++ src/helpers.js
@@ -19,8 +19,10 @@ export function extractRequestFiles (request) {
     if (typeof navigator != 'undefined' && navigator.product == 'ReactNative') {
       isFileList = false
       isFile = typeof node === 'object' && Boolean('name' in node && 'type' in node && 'size' in node && 'path' in node)
-      // React Native requires uri
-      node.uri = node.path
+      if (isFile) {
+        // React Native requires uri
+        node.uri = node.path
+      }
     } else {
       isFileList = node instanceof window.FileList
       isFile = node instanceof window.File
