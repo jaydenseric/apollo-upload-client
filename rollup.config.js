3a5d9703910d9d4728d1743e20ed96d034cdb3b7
giautm <giau.tran@tiki.vn>
2017-06-22 13:41:14 +0700

    revert babel configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
-import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
 
 const pkg = require('./package.json')
 
@@ -9,11 +7,8 @@ export default {
   external: Object.keys(pkg.dependencies),
   plugins: [
     babel({
-      plugins: ['external-helpers'],
-      externalHelpers: true
-    }),
-    nodeResolve(),
-    commonjs()
+      runtimeHelpers: true
+    })
   ],
   targets: [
     {
