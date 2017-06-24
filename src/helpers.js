cc591ba398a89698ac8e598f971b4534ae3355ed
Jayden Seric <me@jaydenseric.com>
2017-06-24 17:15:39 +1000

    Polish.

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,6 @@
-import { isFile } from './file-input'
-
 /**
- * Extracts files from an Apollo Client Request, remembering positions in
- * variables.
+ * Extracts files and their positions within variables from an Apollo Client
+ * request.
  * @see {@link http://dev.apollodata.com/core/apollo-client-api.html#Request}
  * @param {Object} request - Apollo GraphQL request to be sent to the server.
  * @param {Object} request.variables - GraphQL variables map.
@@ -18,22 +16,32 @@ export function extractRequestFiles(request) {
     Object.keys(node).forEach(key => {
       // Skip non-object
       if (typeof node[key] !== 'object' || node[key] === null) return
-      if (isFile(node[key])) {
+
+      // Check if the node is a file
+      if (
+        (typeof File !== 'undefined' && node[key] instanceof File) ||
+        node[key] instanceof ReactNativeFile
+      ) {
         // Extract the file and it's original path in the GraphQL input
         // variables for later transport as a multipart form field.
         files.push({
           variablesPath: `variables${path}.${key}`,
           file: node[key]
         })
+
         // Delete the file from the request variables. It gets repopulated on
         // the server by apollo-upload-server middleware. If an array item it
         // must be deleted without reindexing the array.
         delete node[key]
+
+        // No deeper recursion
         return
       }
+
       // Convert file list to an array so recursion can reach the files
       if (typeof FileList !== 'undefined' && node[key] instanceof FileList)
         node[key] = Array.from(node[key])
+
       // Recurse into child node
       recurse(node[key], `${path}.${key}`)
     })
@@ -43,4 +51,50 @@ export function extractRequestFiles(request) {
   if (request.variables) recurse(request.variables)
 
   return { operation: request, files }
+}
+
+/**
+ * A React Native file.
+ */
+export class ReactNativeFile {
+  /**
+   * A React Native FormData file object.
+   * @see {@link https://github.com/facebook/react-native/blob/v0.45.1/Libraries/Network/FormData.js#L34}
+   * @typedef {Object} ReactNativeFileObject
+   * @property {String} uri - File system path.
+   * @property {String} [type] - File content type.
+   * @property {String} [name] - File name.
+   */
+
+  /**
+   * Constructs a new file.
+   * @param {ReactNativeFileObject} file
+   * @example
+   * const file = new ReactNativeFile({
+   *  uri: uriFromCameraRoll,
+   *  type: 'image/jpeg',
+   *  name: photo.jpg
+   * })
+   */
+  constructor({ uri, type, name }) {
+    this.uri = uri
+    this.type = type
+    this.name = name
+  }
+
+  /**
+   * Creates an array of file instances.
+   * @param {ReactNativeFileObject[]} files
+   * @example
+   * const files = ReactNativeFile.list({
+   *   uri: uriFromCameraRoll1,
+   *   type: 'image/jpeg',
+   *   name: photo-1.jpg
+   * }, {
+   *   uri: uriFromCameraRoll2,
+   *   type: 'image/jpeg',
+   *   name: photo-2.jpg
+   * })
+   */
+  static list = files => files.map(file => new ReactNativeFile(file))
 }
