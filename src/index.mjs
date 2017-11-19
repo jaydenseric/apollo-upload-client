45813aa6a03b1cb3e80c08e2bfe59fe3e8bb4be4
Jayden Seric <me@jaydenseric.com>
2017-11-19 23:32:19 +1100

    Only set populated operation properties.
    Don’t set `operationName` or `variables` if they are empty.

--- src/index.mjs
+++ src/index.mjs
@@ -17,13 +17,13 @@ export const createUploadLink = (
   new ApolloLink(
     ({ operationName, variables, query, extensions, getContext, setContext }) =>
       new Observable(observer => {
-        const requestOperation = {
-          operationName,
-          variables,
-          query: print(query)
-        }
+        const requestOperation = { query: print(query) }
 
-        if (includeExtensions) requestOperation.extensions = extensions
+        if (operationName) requestOperation.operationName = operationName
+        if (Object.keys(variables).length)
+          requestOperation.variables = variables
+        if (extensions && includeExtensions)
+          requestOperation.extensions = extensions
 
         const files = extractFiles(requestOperation)
 
