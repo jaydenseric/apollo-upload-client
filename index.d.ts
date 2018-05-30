c5788b129d9d8c97afa6534e59171ac696d8a9f6
Edward Sammut Alessi <Slessi@users.noreply.github.com>
2018-05-30 09:55:38 +0200

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import { ApolloLink } from "apollo-link";
+import { HttpOptions } from "apollo-link-http-common";
+
+export function createUploadLink(linkOptions?: HttpOptions): ApolloLink;
+
+export * from "extract-files";
