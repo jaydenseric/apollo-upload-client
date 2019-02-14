6c3a9b4fac32238eb60ccf63df632fb836940180
Sze Ka Wai Raymond <szer0601@gmail.com>
2019-02-15 00:30:00 +0800

    #110 Support Apollo Persisted Queries
    Use "query" instead of "body" if the coming query is sent by GET method

--- src/index.js
+++ src/index.js
@@ -151,7 +151,16 @@ exports.createUploadLink = ({
       })
 
       options.body = form
-    } else options.body = payload
+    } else {
+      // Apollo persisted query could be sent using GET method
+      // Request with GET/HEAD method cannot have body
+      // https://github.com/apollographql/apollo-link-persisted-queries#options
+      const method = options.method.toUpperCase()
+      if (method === 'GET' || method === 'HEAD') {
+        // Use query instead for GET/HEAD method
+        options.query = payload
+      } else options.body = payload
+    }
 
     return new Observable(observer => {
       // Allow aborting fetch, if supported.
